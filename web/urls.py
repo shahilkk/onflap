0bdf0109481c091fded83144f9a009eb63bb7b2e
shahilkk <shahilkhanarimbra@gmail.com>
2022-07-16 16:31:10 +0530

    about

--- web/urls.py
+++ web/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name='web'
 
 urlpatterns = [
-    path('',views.base,name='base'),
+    path('base',views.base,name='base'),
+    path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
 ]
      
--- web/views.py
+++ web/views.py
@@ -3,4 +3,15 @@ from .models import *
 
 # Create your views here.
 def base(request):
-    return render(request,"web/base.html")
+    return render(request,"web/partials/base.html")
+
+
+def home(request):
+    return render(request,"web/index.html")    
+
+
+def about(request):
+    return render(request,"web/about.html")        
+
+def contact(request):
+    return render(request,"web/contact.html")        
