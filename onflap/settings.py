ff3269fb661fd12219dc5ee3a443c6f7aa977128
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2022-09-13 17:16:31 +0530

    error 443

--- onflap/settings.py
+++ onflap/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = 'onflap.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    #  'default': {
-    #     "ENGINE": config("DB_ENGINE"),
-    #     "NAME": config("DB_NAME"),
-    #     "USER": config("DB_USER"),
-    #     "PASSWORD": config("DB_PASSWORD"),
-    #     "HOST": config("DB_HOST"),
-    #     'PORT':'5432',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+     'default': {
+        "ENGINE": config("DB_ENGINE"),
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASSWORD"),
+        "HOST": config("DB_HOST"),
+        'PORT':'5432',
+    }
 }
 
 
